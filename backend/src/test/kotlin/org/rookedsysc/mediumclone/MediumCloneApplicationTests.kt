bb2569bae7e71a7b639223f3ef13eb2a2a342460
rookedsysc <rookedsysc36@gmail.com>
2025-04-05 13:43:21 +0900

    feat: create backend project

---
+++ backend/src/test/kotlin/org/rookedsysc/mediumclone/MediumCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.rookedsysc.mediumclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MediumCloneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
