c1ccead399da1bab3b12a6b35690d5547d6c2b74
younaj00 <you.na.joo0@gmail.com>
2025-04-06 22:32:40 +0900

    modify: TanStack React-Router 설정

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,8 @@
+// Router Provider
+
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from './router';
+
+const App = () => <RouterProvider router={router}  />
+
+export default App;
