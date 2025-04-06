f8013c6f3df08b036c9feac5257eb0e46344716e
rookedsysc <rookedsysc36@gmail.com>
2025-04-06 20:40:03 +0900

    feat(DefaultStatus): Security, JWT Status 추가

--- backend/src/main/kotlin/org/rookedsysc/mediumclone/common/status/DefaultStatus.kt
+++ backend/src/main/kotlin/org/rookedsysc/mediumclone/common/status/DefaultStatus.kt
@@ -12,9 +12,32 @@ enum class DefaultStatus(
     SERVER_ERROR(500, 500, "Server Error"),
     NULL_POINT(500, 512, "Null point error"),
 
-    // 제일 마지막에
+    ///# Security
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED.value(), 1401, "Token is expired."),
+    EXPIRED_REQUEST(HttpStatus.REQUEST_TIMEOUT.value(), 1402, "Request is expired."),
+    LOGIN_FAILED(HttpStatus.INTERNAL_SERVER_ERROR.value(), 1403, "Failed to login."),
+    NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND.value(), 1404, "Not found email."),
+    NOT_FOUND_PROVIDER(HttpStatus.NOT_FOUND.value(), 1405, "Not found provider."),
+    WRONG_MODE(HttpStatus.BAD_REQUEST.value(), 1406, "Wrong mode."),
+    UNLINK_FAILED(HttpStatus.INTERNAL_SERVER_ERROR.value(), 1407, "Failed to unlink."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND.value(), 1408, "Not found email."),
+    AUTHENTICATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR.value(), 1409, "Authentication failed."),
     UNDEFINED_STATUS(HttpStatus.INTERNAL_SERVER_ERROR.value(), 999, "Undefined status occurred"),
+
+    NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), 2401, "The token is invalid."),
+    SIGNATURE_NOT_MATCH(HttpStatus.UNAUTHORIZED.value(), 2403, "The token signature does not match."),
+    TOKEN_TYPE_NOT_MATCH(HttpStatus.UNAUTHORIZED.value(), 2405, "The token type does not match."),
+    UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 2406, "An unknown error has occurred."),
+
+    // 권한 없는 토큰
+    UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED.value(), 2407, "The token does not have the required permissions."),
     ;
+
+    companion object {
+        fun fromCustomCode(code: Int): StatusIfs {
+            return entries.firstOrNull { it.getCustomStatusCode() == code } ?: DefaultStatus.UNDEFINED_STATUS
+        }
+    }
 
     override fun getHttpStatusCode(): Int {
         return this.httpStatusCode
