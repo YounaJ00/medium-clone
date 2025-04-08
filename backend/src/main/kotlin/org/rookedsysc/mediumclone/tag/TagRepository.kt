8526552b12fe8e16f9441a790f3f00c77e1d46af
rookedsysc <rookedsysc36@gmail.com>
2025-04-08 21:22:09 +0900

    feat: Recommended Topics API 구현

--- backend/src/main/kotlin/org/rookedsysc/mediumclone/tag/TagRepository.kt
+++ backend/src/main/kotlin/org/rookedsysc/mediumclone/tag/TagRepository.kt
@@ -8,4 +8,14 @@ interface TagRepository : JpaRepository<Tag, Long> {
 
     @Query("SELECT t FROM Tag t WHERE t.name LIKE :keyword%")
     fun searchTagsByNameLike(keyword: String): List<Tag>
+
+    @Query("""
+        SELECT t FROM Tag t 
+        WHERE t.id in (
+            SELECT pt.tagId FROM PostTag pt 
+            GROUP BY pt.tagId
+            ORDER BY COUNT(pt.postId) DESC
+        )
+    """)
+    fun findTop5TagsByPostCount(): List<Tag>
 }
