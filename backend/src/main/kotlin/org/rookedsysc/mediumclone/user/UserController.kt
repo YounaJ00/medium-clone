60af7c3a893e965199d91e03aea39edccee8b8ac
rookedsysc <rookedsysc36@gmail.com>
2025-04-08 21:31:31 +0900

    docs(UserController): Swagger Tag 추가

--- backend/src/main/kotlin/org/rookedsysc/mediumclone/user/UserController.kt
+++ backend/src/main/kotlin/org/rookedsysc/mediumclone/user/UserController.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@io.swagger.v3.oas.annotations.tags.Tag(name = "User API", description = "User 관련 API")
 @RestController
 @RequestMapping("/api/users")
 class UserController(
